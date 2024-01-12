bae166a74e8d799ae9b01d73f599a22a069f3357
anandu-m-s <156334077+anandu-m-s@users.noreply.github.com>
2024-01-12 13:23:44 +0530

    Createed  api_call.py file

---
+++ api_call.py
@@ -0,0 +1,18 @@
+import requests
+import os
+ def main():
+   api_url="http://localhost:8082/doc/demo/create/pkpzip"
+   repo_name=os.getenv("INPUT_REPO_NAME")
+   json_file_path=os.getenv("INPUT_JSON_FILE_PATH")
+
+with open(json_file_path,"rb") as file:
+  response= requests.post(
+    api_url,
+    headers={"Content-Type":"application/json"},
+    data=file
+  )
+
+print(response.text)
+
+if __name__ == "__main__":
+  main()
