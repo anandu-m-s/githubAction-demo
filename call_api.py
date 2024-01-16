e522a5c04353ec2639815361aac61eadf2f418a0
anandu-m-s <156334077+anandu-m-s@users.noreply.github.com>
2024-01-16 21:58:45 +0530

    Update call_api.py

--- call_api.py
+++ call_api.py
@@ -4,16 +4,16 @@ def main():
    print("Hello World")
    api_url="http://localhost:8082/doc/demo/create/pkpzip"
    repo_name=os.getenv("INPUT_REPO_NAME")
-   json_file_path=os.getenv("INPUT_JSON_FILE_PATH")
+   # json_file_path=os.getenv("INPUT_JSON_FILE_PATH")
 
-with open(json_file_path,"rb") as file:
-  response= requests.post(
-    api_url,
-    headers={"Content-Type":"application/json"},
-    data=file
-  )
+# with open(json_file_path,"rb") as file:
+#   response= requests.post(
+#     api_url,
+#     headers={"Content-Type":"application/json"},
+#     data=file
+#   )
 
-print(response.text)
+print(repo_name.text)
 
 if __name__ == "__main__":
   main()
