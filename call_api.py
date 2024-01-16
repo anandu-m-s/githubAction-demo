cb4e7ed0e884d5160a962b9998bd6d43f8d27156
anandu-m-s <156334077+anandu-m-s@users.noreply.github.com>
2024-01-16 22:14:27 +0530

    Update call_api.py

--- call_api.py
+++ call_api.py
@@ -3,7 +3,9 @@ import os
 def main():
    print("Hello World")
    api_url="http://localhost:8082/doc/demo/create/pkpzip"
-   repo_name=os.getenv("INPUT_REPO_NAME")
+   repo_name = sys.argv[1]
+   json_file_path = sys.argv[2]
+   # repo_name=os.getenv("INPUT_REPO_NAME")
    # json_file_path=os.getenv("INPUT_JSON_FILE_PATH")
 
 # with open(json_file_path,"rb") as file:
