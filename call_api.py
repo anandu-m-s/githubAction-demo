5e79e12d75d8d024dd3789a9686a40080e5830ff
anandu-m-s <156334077+anandu-m-s@users.noreply.github.com>
2024-01-17 18:59:40 +0530

    Update call_api.py

--- call_api.py
+++ call_api.py
@@ -9,12 +9,12 @@ def main():
    print(api_url)
    print(json_file_path)
 
-# with open(json_file_path,"rb") as file:
-#   response= requests.post(
-#     api_url,
-#     headers={"Content-Type":"application/json"},
-#     data=file
-#   )
+   with open('pkp.json',"rb") as file:
+     response= requests.post(
+     api_url,
+     headers={"Content-Type":"application/json"},
+     data=file
+   )
 
 if __name__ == "__main__":
   main()
