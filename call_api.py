d9efb5247bd0be19f77793a1d0ad0d6f7585ff49
anandu-m-s <156334077+anandu-m-s@users.noreply.github.com>
2024-01-17 19:11:37 +0530

    Update call_api.py

--- call_api.py
+++ call_api.py
@@ -3,18 +3,21 @@ import os
 import sys
 def main():
    print("Hello World")
-   api_url="http://localhost:8082/doc/demo/create/pkpzip"
+   # api_url="http://localhost:8082/doc/demo/create/pkpzip"
    
-   json_file_path=os.getenv("INPUT_JSON_FILE_PATH")
-   print(api_url)
-   print(json_file_path)
+   # json_file_path=os.getenv("INPUT_JSON_FILE_PATH")
+   # print(api_url)
+   # print(json_file_path)
 
-   with open('pkp.json',"rb") as file:
-     response= requests.post(
-     api_url,
-     headers={"Content-Type":"application/json"},
-     data=file
-   )
+   # with open('pkp.json',"rb") as file:
+   #   response= requests.post(
+   #   api_url,
+   #   headers={"Content-Type":"application/json"},
+   #   data=file
+   # )
+   url="http://localhost:8082/doc/demo/get"
+   r= requests.get(url)
+   print(r)
 
 if __name__ == "__main__":
   main()
